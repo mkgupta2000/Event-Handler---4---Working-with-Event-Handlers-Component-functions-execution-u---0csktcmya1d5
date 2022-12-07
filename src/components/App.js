03a929ad7ad3f69e72a8dae2ab2fc2196375d5ba
mkgupta2000 <105657874+mkgupta2000@users.noreply.github.com>
2022-12-07 21:17:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,25 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 const App = () => {
+  const handleDoubleClick = (event) => {
+    console.log("I was double clicked");
+  };
 
-  const handleDoubleClick = (event) =>{
-  
+  function singel() {
+    console.log("I was not double clicked");
   }
+
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button
+        id="dblclick-btn"
+        onClick={singel}
+        onDoubleClick={handleDoubleClick}
+      >
+        Double click me
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
